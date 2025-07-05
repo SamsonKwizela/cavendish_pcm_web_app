0870c6d67c10024c6efb00d7d9946af0bd5b2258
Desire <desireirankundawilliams@gmail.com>
2025-07-05 20:11:58 +0200

    create new components

--- cavendish/src/App.jsx
+++ cavendish/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import Header from "./pages/Header";
+import Home from "./pages/Home";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
+      <ContactUs />
       {/* <LandingPage /> */}
     </div>
   );
---
+++ cavendish/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Home extends Component {
+  render() {
+    return <div>welcome homw</div>;
+  }
+}
+
+export default Home;
