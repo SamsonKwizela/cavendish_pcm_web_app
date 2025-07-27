ecd6f004f8643cbdd40c8a62edc5239cf7921e37
Desire <desireirankundawilliams@gmail.com>
2025-07-27 21:05:58 +0200

    added a navigation bar

--- cavendish/src/App.jsx
+++ cavendish/src/App.jsx
@@ -6,19 +6,27 @@ import { Route, Routes } from "react-router-dom";
 import AboutUs from "./pages/AboutUs";
 import NotFound from "./pages/NotFound";
 import Footer from "./pages/Footer";
+import { createTheme, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { NavigationBar } from "./component/NavigationBar";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 function App() {
   return (
-    <div>
-      <Header />
+    <MantineProvider theme={theme}>
+      <NavigationBar />
+      {/* <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
-    </div>
+      <Footer /> */}
+    </MantineProvider>
   );
 }
 
